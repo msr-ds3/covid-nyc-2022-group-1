0f9ec50c49b29734c360c82913fd9a7abe75fcb6
ShanaFarber <shoshanafarber1@gmail.com>
2022-06-23 21:16:17 +0000

    final

--- 01_download_nyc_zip_codes.sh
+++ 01_download_nyc_zip_codes.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
-# downloads nyc zipcodes
 #
 
+# Make a Data directory
+mkdir Data
+
+# Downloads nyc zipcodes into Data directory
 curl https://raw.githubusercontent.com/erikgregorywebb/nyc-housing/master/Data/nyc-zip-codes.csv > Data/nyc-zip-codes.csv
