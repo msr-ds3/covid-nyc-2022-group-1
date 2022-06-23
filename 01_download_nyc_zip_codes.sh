e925c1c645710c321d76f1eca1a1b840e34eb6f3
ShanaFarber <shoshanafarber1@gmail.com>
2022-06-23 20:00:45 +0000

    covid

--- 01_download_nyc_zip_codes.sh
+++ 01_download_nyc_zip_codes.sh
@@ -3,4 +3,4 @@
 # downloads nyc zipcodes
 #
 
-curl https://raw.githubusercontent.com/msr-ds3/covid-nyc-2022-group-1/main/Data/nyc-zip-codes.csv > nyc-zip-codes.csv
+curl https://github.com/erikgregorywebb/nyc-housing/blob/master/Data/nyc-zip-codes.csv > Data/nyc-zip-codes.csv
--- 02_download_april_1_cases.sh
+++ 02_download_april_1_cases.sh
@@ -3,4 +3,5 @@
 # downloads April 1 cases by zip 
 #
 
-curl 01_download_nyc_zip_codes.sh > april-1-cases.csv
+curl https://raw.githubusercontent.com/nychealth/coronavirus-data/097cbd70aa00eb635b17b177bc4546b2fce21895/tests-by-zcta.csv > Data/april-1-cases.csv
+
